52bd28fe27d8b6299743fc187cc71499f4f6091f
Anna Tarankova <tarankovaanna@gmail.com>
2023-03-22 16:31:26 +0300

    Change main

--- src/main/java/ru/netology/domain/NoOneIsRegisteredException.java
+++
@@ -1,7 +0,0 @@
-package ru.netology.domain;
-
-public class NoOneIsRegisteredException extends RuntimeException {
-    public NoOneIsRegisteredException() {
-        super("Нет зарегистрированных игроков");
-    }
-}
